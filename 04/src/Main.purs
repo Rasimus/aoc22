7f91aefc48c6b4816328a9df6ae4ca891fd783b9
Rasimus <sebastian.rasimus@gmail.com>
2022-12-04 20:06:38 +0100

    Day 4 Part 2 completed.

--- 04/src/Main.purs
+++ 04/src/Main.purs
@@ -2,26 +2,34 @@ module Main where
 
 import Prelude
 
+import Data.Array (many, filter, length)
 import Data.Either (Either(..))
+import Data.Tuple (Tuple(..), uncurry)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Data.Tuple (Tuple(..), uncurry)
 import Parsing (runParser, Parser)
+import Parsing.Combinators (optional)
 import Parsing.String (char)
 import Parsing.String.Basic (number)
-import Parsing.Combinators (optional)
-import Data.Array (many, filter, length)
+
 type Range = {start :: Number, end :: Number}
 
 contains :: Range -> Range -> Boolean
 contains a b | a.start <= b.start && a.end >= b.end = true
              | otherwise = false
+overlapping :: Range -> Range -> Boolean
+overlapping a b | contains a {start: b.start, end: b.start} ||
+                  contains a {start: b.end, end: b.end} ||
+                  contains b {start: a.start, end: a.start} ||
+                  contains b {start: a.end, end: a.end} = true
+                | otherwise = false
 
 pairContains :: Tuple Range Range -> Boolean
 pairContains (Tuple a b) = contains a b || contains b a
+
 lineParser :: Parser String (Tuple Range Range)
 lineParser = do
   firstRange <- rangeParser
@@ -38,11 +46,14 @@ rangeParser = do
   pure ({start: start, end: end})
 
 
-
-
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "input.txt"
   case runParser input $ many lineParser of
-    Right xs -> logShow $ length $ filter ((==) true) $ map pairContains xs
+    Right xs -> do
+      log "Part 1. Number of fully contained pairs:"
+      logShow $ countTrue $ map pairContains xs
+      log "Part 2. Number of overlapping pairs:"
+      logShow $ countTrue $ map (uncurry overlapping) xs
     Left msg -> logShow msg
+  where countTrue xs = length $ filter ((==) true) xs
